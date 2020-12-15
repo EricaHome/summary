5a67705426ae1ac7bb3806fe6e4f3696ab607391
zhangting <187268218@qq.com>
2020-12-15 09:00:33 +0800

    技术总结

---
+++ src/test/java/com/erica/summary/SummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erica.summary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
